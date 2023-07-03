a632098cfb24047b463b4bfff82007aa6220ae8f
mfeaster18 <livefree.maryf@gmail.com>
2023-07-02 21:33:17 -0400

    Third commit

--- PyBank/working_pybank.py
+++ PyBank/working_pybank.py
@@ -26,13 +26,29 @@ import csv
 
 rowCount = 0
 netTotal = 0
+lastProfLoss = 0
+currentProfLoss = 0
+totalAverage = 0
+greatestGain = 0
+greatestGainDate = ""
+greatestDecrease = 0
+greatestDecreaseDate = ""
 
 
-
+# connect my python file to the csv file using the path join function, set csv to read and store headers to skip them in the for loop
 
 budget_data = os.path.join("Resources", "budget_data.csv")
 with open(budget_data) as csv_file:
     reader = csv.reader(csv_file)
+    headers = next(budget_data, None)
     
-#increment the total number of months in the data set
+#for loop to perform the calculations needed for final figures
+    for row in reader:
+        #use the row count variable to calculate number of months in the data set
         rowCount += 1 
+        #use the net total variable to calculate the total profit and loss for the data set
+        netTotal += float(row[1])
+        #set up loop to get averages and greatest profit and loss
+        currentProfLoss = float(row[1]) - lastProfLoss
+        if rowCount > 1:
+            

5727a988c524a961d62cbb2b8b5232406c9dc6d1
mfeaster18 <livefree.maryf@gmail.com>
2023-07-05 12:56:00 -0400

    Second commit to PyPoll- wrote most of my code, need to finish the print statement and make the text file.

---
+++ PyBank/working_pybank.py
@@ -0,0 +1,88 @@
+# In this Challenge, you are tasked with creating a Python script
+# to analyze the financial records of your company. You will be
+# given a financial dataset called budget_data.csv. The dataset
+# is composed of two columns: "Date" and "Profit/Losses".
+
+# Your task is to create a Python script that analyzes the records
+# to calculate each of the following values:
+
+# The total number of months included in the dataset
+
+# The net total amount of "Profit/Losses" over the entire period
+
+# The changes in "Profit/Losses" over the entire period, and then
+# the average of those changes
+
+# The greatest increase in profits (date and amount) over the
+# entire period
+
+# The greatest decrease in profits (date and amount) over the
+# entire period
+
+import os
+import csv
+
+# Make variables to analyze the dataset
+
+rowCount = 0
+netTotal = 0
+lastProfLoss = 0
+currentProfLoss = 0
+totalAverage = 0
+greatestGain = 0
+greatestGainDate = ""
+greatestDecrease = 0
+greatestDecreaseDate = ""
+
+
+# connect my python file to the csv file using the path join function, set csv to read and store headers to skip them in the for loop
+
+budget_data = os.path.join("Resources", "budget_data.csv")
+with open(budget_data) as csv_file:
+    reader = csv.reader(csv_file)
+    headers = next(reader)
+
+# for loop to perform the calculations needed for final figures
+    for row in reader:
+        # use the row count variable to calculate number of months in the data set
+        rowCount += 1
+        # use the net total variable to calculate the total profit and loss for the data set
+        netTotal += float(row[1])
+        # set up loop to get averages and greatest profit and loss
+        currentProfLoss = float(row[1]) - lastProfLoss
+        if rowCount > 1:
+            totalAverage += currentProfLoss
+        # update figures
+        lastProfLoss = float(row[1])
+        # get the greatest gain values
+        if currentProfLoss > greatestGain:
+            greatestGain = currentProfLoss
+            greatestGainDate = row[0]
+        # get the greatest decrease values
+        elif currentProfLoss < greatestDecrease:
+            greatestDecrease = currentProfLoss
+            greatestDecreaseDate = row[0]
+
+
+# Print all the data gathered into the format provided to follow
+
+print("Financial Analysis \n----------------------------")
+print(f"Total Months: {rowCount}"),
+print(f"Total: ${netTotal:.0f}"),
+print(f"Average Change: ${(totalAverage / (rowCount - 1)):.2f}"),
+print(
+    f"Greatest Increase in Profits: {greatestGainDate} (${greatestGain:.0f})"),
+print(
+    f"Greatest Decrease in Profits: {greatestDecreaseDate} (${greatestDecrease:.0f})")
+
+# Make and write into the analysis text file
+
+with open('analysis.txt', 'w') as f:
+    print("Financial Analysis \n----------------------------", file=f)
+    print(f"Total Months: {rowCount}", file=f)
+    print(f"Total: ${netTotal:.0f}", file=f)
+    print(f"Average Change: ${(totalAverage / (rowCount - 1)):.2f}", file=f)
+    print(
+        f"Greatest Increase in Profits: {greatestGainDate} (${greatestGain:.0f})", file=f)
+    print(
+        f"Greatest Decrease in Profits: {greatestDecreaseDate} (${greatestDecrease:.0f})", file=f)
---
+++ PyPoll/main.py
@@ -0,0 +1,67 @@
+#In this Challenge, you are tasked with helping a small, rural 
+#town modernize its vote-counting process.
+
+#You will be given a set of poll data called election_data.csv. 
+#The dataset is composed of three columns: "Voter ID", "County", 
+#and "Candidate". Your task is to create a Python script that 
+#analyzes the votes and calculates each of the following values:
+
+#The total number of votes cast
+
+#A complete list of candidates who received votes
+
+#The percentage of votes each candidate won
+
+#The total number of votes each candidate won
+
+#The winner of the election based on popular vote
+
+import os
+import csv
+
+# Make variables to analyze the dataset
+
+
+totalVotes = 0
+stockham = "Charles Casper Stockham"
+stockhamVotes = 0
+degette = "Diana DeGette"
+degetteVotes = 0
+doane = "Raymon Anthony Doane"
+doaneVotes = 0
+popularVote = ""
+
+# connect my python file to the csv file using the path join function, set csv to read and store headers to skip them in the for loop
+
+election_data = os.path.join("Resources", "election_data.csv")
+with open(election_data) as csv_file:
+    reader = csv.reader(csv_file)
+    headers = next(reader)
+
+# make variable to track total votes
+
+    for row in reader:
+        totalVotes += 1
+        if row[2] == stockham:
+            stockhamVotes += 1
+        elif row[2] == degette:
+            degetteVotes += 1
+        else:
+            doaneVotes += 1
+
+# get the percentages of all the canidates votes
+
+stockhamPercentage = stockhamVotes / totalVotes
+degettePercentage = degetteVotes / totalVotes   
+doanePercentage = doaneVotes / totalVotes
+
+
+# use all the data gathered from the python script to find the popular vote
+
+if stockhamVotes > degetteVotes and stockhamVotes > doaneVotes:
+    popularVote  = stockham
+elif degetteVotes > stockhamVotes and degetteVotes > doaneVotes:
+    popularVote = degette
+else:
+    popularVote = doane
+
